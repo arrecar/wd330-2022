dad941f1a68d1fc934c5f85940289c3284e92185
Poser <arr15004@byui.edu>
2022-02-05 18:02:09 -0600

    week4

---
+++ week4/main.js
@@ -0,0 +1,36 @@
+//Vector type
+console.log("A vector type");
+
+class Vector {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    plus(other) {
+      return new Vector(this.x + other.x, this.y + other.y);
+    }
+  
+    minus(other) {
+      return new Vector(this.x - other.x, this.y - other.y);
+    }
+  
+    get length() {
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+  }
+  
+  console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+  // → Vec{x: 3, y: 5}
+  console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+  // → Vec{x: -1, y: -1}
+  console.log(new Vector(3, 4).length);
+  // → 5
+
+  //Borrowing a method
+  console.log("Borrowing a method");
+
+  let map = {one: true, two: true, hasOwnProperty: true};
+
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
+// → true
